ef4cb8c086eeb721c787a93478e6787706880485
solalem <solalem2001@gmail.com>
2023-10-03 13:59:51 +0300

    Create page

--- Clinic.Web/Areas/Appointments/Patients/Pages/Create.razor.cs
+++ Clinic.Web/Areas/Appointments/Patients/Pages/Create.razor.cs
@@ -1,4 +1,41 @@
-namespace Clinic.Web.Areas.Appointments.Patients.Pages
-{
+using Clinic.ViewModels.Appointments.Patients;
+using Clinic.Web.Components;
+using Microsoft.AspNetCore.Components;
 
+namespace Clinic.Web.Areas.Appointments.Pages.Patients
+{
+    public partial class Create: BlazorPage
+    {
+        [Inject]
+        public IAppointmentsServices AppointmentsServices { get; set; }
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        private CreatePatient Item = new CreatePatient();
+
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                CallRequestRefresh();
+            }
+
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+        private async Task CreateClick()
+        {
+            try
+            {
+                var result = await AppointmentsServices.PatientService.CreateAsync(Item);
+
+                NavigationManager.NavigateTo($"Appointments/Patients/Index?HighlightId={result.Id}");
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+    }
 }
